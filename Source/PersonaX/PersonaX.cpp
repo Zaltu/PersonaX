91f26351a9065453d7d9457c23e977f174a0c0ae
Zaltu <swwouf@hotmail.com>
2019-02-07 22:13:28 -0500

    Initial commit

---
+++ Source/PersonaX/PersonaX.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "PersonaX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PersonaX, "PersonaX" );
+ 
---
+++ Source/PersonaX/PersonaXGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PersonaXGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APersonaXGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APersonaXGameMode();
+};
+
+
+
