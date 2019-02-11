f96928e8bf1ccb8a97769b366b1d2653f601ea77
Zaltu <swwouf@hotmail.com>
2019-02-11 18:15:50 -0500

    dunno

---
+++ Source/PersonaX/Public/AbstractLevel.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PERSONAX_API AbstractLevel
+{
+public:
+	AbstractLevel();
+	~AbstractLevel();
+};
